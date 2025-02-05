a8c4985d9024eb02adecc3142f4bc9643984af8b
Sujal Verma <vermasujal16@gmail.com>
2025-02-05 18:43:46 +0530

    auth done

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -2,11 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import userModel,{IUser} from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
-import jwt,{Secret} from "jsonwebtoken"
+import jwt,{JwtPayload, Secret} from "jsonwebtoken"
 import ejs from 'ejs'
 import path from 'path'
 import sendMail from "../utils/sendMail";
-import { sendToken } from "../utils/jwt";
+import { accessTokenOptions, refreshTokenOptions, sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 interface IRegistrationBody{
     name: string;
@@ -69,7 +70,7 @@ export const createActivationToken=(user:any):IActivationToken=>{
         expiresIn: "5m"
     }
 )
-return {token,activationCode}
+return {token,activationCode} 
 }
 //activate user
 interface IActivationRequest{
@@ -138,9 +139,43 @@ export const logoutUser = CatchAsyncError(async(req:Request,res:Response,next:Ne
     try{
         res.cookie("access_token","",{maxAge:1});
         res.cookie("refresh_token","",{maxAge:1});
+        const userId=req.user?._id || "";
+        //delete the user info from redis database
+        redis.del(userId);
         res.status(200).json({success:true,message:"Loggesd out successfully"});
     }
     catch(error:any){
+        return next(new ErrorHandler(error.message,400));
+    }
+});
+export const updateAccessToken = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const refresh_token = req.cookies.refresh_token as string;
+        const decoded = jwt.verify(refresh_token,process.env.REFRESH_TOKEN as string) as JwtPayload;
+        const message = 'Could not refresh token';
+        if(!decoded){
+            return next (new ErrorHandler(message,400));
+        }
+        const session = await redis.get(decoded.id as string);
+        if(!session){
+            return next(new ErrorHandler(message,400));
+        }
+        const user = JSON.parse(session);
+        const accessToken = jwt.sign({id:user._id},process.env.ACCESS_TOKEN as string,{
+            expiresIn: "5m",
+        })
+        const refreshToken = jwt.sign({id:user._id},process.env.REFRESH_TOKEN as string,{
+            expiresIn: "3d",
+        })
+        res.cookie("access_token",accessToken,accessTokenOptions);
+        res.cookie("refresh_token",refreshToken,refreshTokenOptions);
+
+        res.status(200).json({
+            status: "success",
+            accessToken,
+        })
+
+    } catch (error:any) {
         return next(new ErrorHandler(error.message,400));
     }
 });
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -11,40 +11,36 @@ interface ITokenOptions {
     sameSite:"lax"|"strict"|"none"|undefined;
     secure?:boolean;
 }
+const accessTokenExpire=parseInt(
+    process.env.ACCESS_TOKEN_EXPIRE||"300",
+    10
+)
+const refreshTokenExpire=parseInt(
+    process.env.REFRESH_TOKEN_EXPIRE||"1200",
+    10
+)
 
+
+//parse env variable to integrate
+export const accessTokenOptions:ITokenOptions={
+    expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+    maxAge: accessTokenExpire * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax"
+};
+
+export const refreshTokenOptions:ITokenOptions={
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax"
+};
 export const sendToken=(user:IUser,statusCode:number,res:Response) =>{
     const accessToken = user.SignAccessToken();
     const refreshToken = user.SignRefreshToken();
-
+    //uploading session to the redis database
     redis.set(String(user._id), JSON.stringify(user));
-
-
-
-    const accessTokenExpire=parseInt(
-        process.env.ACCESS_TOKEN_EXPIRE||"300",
-        10
-    )
-    const refreshTokenExpire=parseInt(
-        process.env.REFRESH_TOKEN_EXPIRE||"1200",
-        10
-    )
-    
-    
-    //parse env variable to integrate
-    const accessTokenOptions:ITokenOptions={
-        expires: new Date(Date.now() + accessTokenExpire * 1000),
-        maxAge: accessTokenExpire*1000,
-        httpOnly: true,
-        sameSite: "lax"
-    };
-
-    const refreshTokenOptions:ITokenOptions={
-        expires: new Date(Date.now() + refreshTokenExpire * 1000),
-        maxAge: refreshTokenExpire*1000,
-        httpOnly: true,
-        sameSite: "lax"
-    };
-    
+    // only set secure to true in production
     if(process.env.NODE_ENV==='production'){
         accessTokenOptions.secure=true;
     }
