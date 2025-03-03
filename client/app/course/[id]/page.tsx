55b39d7c9537779ebd1271b79bd26d1f40dbf19f
Sujal Verma <vermasujal16@gmail.com>
2025-03-03 23:16:42 +0530

    bugg fix

--- client/app/course/[id]/page.tsx
+++ client/app/course/[id]/page.tsx
@@ -1,20 +1,24 @@
-'use client'
+"use client";
 import React from "react";
 import CourseDetailsPage from "../../components/Course/CourseDetailsPage";
 import { useParams } from "next/navigation";
 
 const Page = () => {
-    const params = useParams(); // Correct way to access params in client components
-
+    const params = useParams(); 
+    
     if (!params?.id) {
         return <div>Loading...</div>; // Handle undefined params
     }
+
+    const id = Array.isArray(params.id) ? params.id[0] : params.id; // ✅ Ensure id is always a string
+
     return (
         <div>
-            <CourseDetailsPage id={params.id} />
+            <CourseDetailsPage id={id} /> {/* ✅ Now id is always a string */}
         </div>
-    )
-}
+    );
+};
 
 export default Page;
+
  
