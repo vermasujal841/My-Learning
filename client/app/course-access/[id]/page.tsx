9c4077b9adb04190d295b40afc95652b29a2658c
Sujal Verma <vermasujal16@gmail.com>
2025-03-03 23:09:22 +0530

    bug fix

--- client/app/course-access/[id]/page.tsx
+++ client/app/course-access/[id]/page.tsx
@@ -10,7 +10,7 @@ import { useParams } from "next/navigation";
 
 const Page = () => {
   const params = useParams();
-    const id = params?.id;
+  const id = Array.isArray(params?.id) ? params.id[0] : params?.id || "";
   const { isLoading, error, data,refetch } = useLoadUserQuery(undefined, {});
 
   useEffect(() => {
